fd2486f513e2d876d62a41992a03086f9c91814d
Rob McDonald <rob.a.mcdonald@gmail.com>
2025-09-01 13:05:51 -0700

    Fix transformation matrix again

--- matlab/build_T_strain.m
+++ matlab/build_T_strain.m
@@ -7,7 +7,7 @@ function T = build_T_strain( theta_deg )
     T = [    m * m,     n * n,   0, 0, 0,       m * n;
              n * n,     m * m,   0, 0, 0,      -m * n;
                  0,         0,   1, 0, 0,           0;
-                 0,         0,   0, m, n,         0;
-                 0,         0,   0, -n,  m,         0;
+                 0,         0,   0, m, -n,         0;
+                 0,         0,   0, n,  m,         0;
         -2.0 * m * n, 2.0 * m * n, 0, 0, 0, m * m - n * n ];
 end
--- python/lamtheory3d.py
+++ python/lamtheory3d.py
@@ -29,8 +29,8 @@ def build_T_strain(theta_deg: float) -> np.ndarray:
         [    m*m,     n*n, 0., 0., 0.,     m*n],
         [    n*n,     m*m, 0., 0., 0.,    -m*n],
         [    0.,       0., 1., 0., 0.,       0.],
-        [    0.,       0., 0., m,  n,       0.],
-        [    0.,       0., 0., -n,  m,       0.],
+        [    0.,       0., 0., m,  -n,       0.],
+        [    0.,       0., 0., n,   m,       0.],
         [-2.0*m*n, 2.0*m*n, 0., 0., 0., m*m - n*n]
     ], dtype=float)
     return T
