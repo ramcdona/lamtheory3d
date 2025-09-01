a4d620d951b75c360e7e01f5ad827d55686b7d60
Rob McDonald <rob.a.mcdonald@gmail.com>
2025-09-01 12:23:46 -0700

    Correct strain transformation matrix

--- matlab/build_T_strain.m
+++ matlab/build_T_strain.m
@@ -4,10 +4,10 @@ function T = build_T_strain( theta_deg )
     m = cos(th);
     n = sin(th);
 
-    T = [    m * m,     n * n,   0, 0, 0,      -m * n;
-             n * n,     m * m,   0, 0, 0,       m * n;
+    T = [    m * m,     n * n,   0, 0, 0,       m * n;
+             n * n,     m * m,   0, 0, 0,      -m * n;
                  0,         0,   1, 0, 0,           0;
-                 0,         0,   0, m, -n,         0;
-                 0,         0,   0, n,  m,         0;
+                 0,         0,   0, m, n,         0;
+                 0,         0,   0, -n,  m,         0;
         -2.0 * m * n, 2.0 * m * n, 0, 0, 0, m * m - n * n ];
 end
